c53514fd70b2862a83c583714d3a389c68081f47
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-05-17 15:43:01 -0300

    terminar o ultimo metodo

--- src/models/generatormission.ts
+++ src/models/generatormission.ts
@@ -1,12 +1,16 @@
-import { ShipTypes, starShipsNames } from "src/utils/types"
+import { PlanetsName, PlanetType, ShipTypes, starShipsNames } from "src/utils/types"
 import { Planet } from "./planet"
 import { StarShip } from "./starship"
 import { randomChoice } from "src/utils/randomChoice"
 import { CreateStarShip } from "src/services/createStarShip"
+import { CreatePlanet } from "src/services/createPlanet"
+import { Cargo } from "./cargo"
 
 export class GeneratorMission {
 
     private static readonly shipTypes: ShipTypes[] = Object.keys(starShipsNames) as ShipTypes[]
+
+    private static readonly configPlanet: PlanetType[] = Object.keys(PlanetsName) as PlanetType[]
 
 
     public generateRandomShips(): StarShip {
@@ -21,6 +25,18 @@ export class GeneratorMission {
 
 
     public generateRandomPlanets(): Planet {
+        const randomOptions = randomChoice(GeneratorMission.configPlanet) as PlanetType
+        const planetNames = PlanetsName[randomOptions]
+        const randomName = randomChoice(planetNames)
+
+        const factoryTypePlanets = `create${randomOptions}Planet` as keyof typeof CreatePlanet
+        const factoryCreatePlanets = CreatePlanet[factoryTypePlanets] as (name: string) => Planet
+
+        return factoryCreatePlanets(randomName)
+    }
+
+
+    public generateRandomCargos(): Cargo {
 
     }
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,7 @@
 
 export type CargoType = 'INDUSTRIAL_EQUIPMENT' | 'BIOTECH' | 'DROID_PARTS' 
 
-export type PlanetType = 'AQUATIC' | 'ROCKY' | 'CORROSIVE'
+export type PlanetType = keyof PlanetsName
 
 export type Requirements = 'SHOCK_PROOF' | 'PRESSURE_SEALED' | 'ANTI_CORROSIVE'
 
@@ -21,4 +21,18 @@ export const starShipsNames: ShipsName = {
     StarDestroyer: ['Executor', 'Devastator', 'Malgus'],
     TieExplorer: ['Phantom','Shadow Probe', 'Deep Scanner'],
     GR75: ['Dawn Runner', 'Light Carrier', 'Rebel Hauler']
+}
+
+
+export type PlanetsName = {
+    ROCKY: string[],
+    CORROSIVE: string[],
+    AQUATIC: string[]
+}
+
+
+export const PlanetsName: PlanetsName = {
+    ROCKY: ['Korriban', 'Jaku', 'Geonosis'],
+    CORROSIVE: ['Dagobah', 'Vjun', 'Felucia'],
+    AQUATIC: ['Mon Cala', 'Kamino', 'Naboo']
 }
