8da36c9029f7da2f159cb68eedf1cb8b296e492b
Gabriel Oliveira Pinto <157530699+gaboliveira-alt@users.noreply.github.com>
2025-05-18 18:41:19 -0300

    ajuste final sendo feito

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,11 @@ const mission2 = new StarTrip()
 mission2.startMission()
 mission2.executeMission()
 
+const mission3 = new StarTrip()
+mission3.startMission()
+mission3.executeMission()
+
 console.log(mission.generateReport())
 console.log(mission1.generateReport())
 console.log(mission2.generateReport())
+console.log(mission3.generateReport())
